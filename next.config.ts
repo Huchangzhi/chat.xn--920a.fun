4412084cebc58c1944da70522dbec59dac222b9b
huchangzhi <hcz1017@outlook.com>
2026-02-19 23:29:08 +0800

    bug

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  typedRoutes: true,
-  reactCompiler: true,
   experimental: {
     typedEnv: true,
   },
